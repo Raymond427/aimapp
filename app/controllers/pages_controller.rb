bd9ebd0703ee6eaa9c98d0f1c37da29dcdab5b79
Raymond Ferrell <rferrel427@gmail.com>
2015-06-27 12:58:36 -0400

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+  
+  def erb_demo
+  end
+  
+  def Get_Involved
+  end
+
+  def Calendar
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+niner_nights = News.create( title: 'Niner Nights', date: 'Summer 2014' ,thumburl: 'https://d1q9wbuypc40mm.cloudfront.net/qcb575tusx9el2h.jpg', description: 'Come out to visit the AIM booth during Niner Nights on the UNC Charlotte campus! future freshman signed AIM\'s door to "open your door from dreams to reality!"')
