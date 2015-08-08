75d75652cf0978ee79606beabef2a4f45e7ffb32
Raymond Ferrell <rferrel427@gmail.com>
2015-08-08 16:18:05 -0400

    made controllers DRYer

--- db/migrate/20150628161500_create_members.rb
+++ db/migrate/20150628161500_create_members.rb
@@ -3,8 +3,8 @@ class CreateMembers < ActiveRecord::Migration
     create_table :members do |t|
       t.string :first_name
       t.string :last_name
-      t.text :thumb_url
-      t.text :linkedin_url
+      t.string :thumb_url
+      t.string :linkedin_plug
       t.string :major
       t.string :email
       t.string :graduation_date
