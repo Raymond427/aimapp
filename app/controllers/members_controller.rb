971b284df1ff85acdb7ac55d06e0c719ad47c488
Raymond Ferrell <rferrel427@gmail.com>
2016-06-02 01:25:42 -0400

    Deleted AJAX attempt and added Google Analytics Tag Manager

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -48,10 +48,6 @@ class MembersController < ApplicationController
   def destroy
     @members.destroy
     redirect_to({ action: 'executives' }, alert: 'The member has been deleted!') #change for members
-
-    respond_to do |format|
-      format.js
-    end
   end
 
   def executives
